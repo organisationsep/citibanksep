eb75979c94a39eaf094b50db1b050a57dc14675e
shaikshab <shaikshab1234@gmail.com>
2022-12-10 15:53:01 +0530

    committed again

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+iam add this file by checking git reset command 
 hello again iam updating this file 
+
 this file contains the deployement
 
---
+++ test.sh
@@ -0,0 +1,1 @@
+this file contAINS THE SHELLSCRIPT FILE

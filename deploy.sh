6e714a568848b01dfa438b637eb6a913970a4207
shaikshab <114868530+shaikshab@users.noreply.github.com>
2022-12-14 20:15:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+hello iam updating again, is it possible to push the code from our side without getting the updating code to other side..{121}
 iam updating development branch
 hello again iam updating this file
 this file contains the deployement

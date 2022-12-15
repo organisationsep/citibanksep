faa3b7889b127856ab9f6ed4195f613dd6083667
shaikshab <114868530+shaikshab@users.noreply.github.com>
2022-12-15 15:41:57 +0530

    Update DButils.sh

--- DButils.sh
+++ DButils.sh
@@ -1,3 +1,4 @@
 I am updating thi line by git clone url
 iam updating in development branch
 this file contains the dbutils.sh files
+iam using pull request in remote repository dev updated content want to see in master
